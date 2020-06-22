567b62e0ecd82ecf272095a1655b17111cd5b038
Ramdhakal <65497745+Ramdhakal@users.noreply.github.com>
2020-06-22 20:28:19 +0545

    Add files via upload

---
+++ index.php
@@ -0,0 +1,254 @@
+
+	<!DOCTYPE html>
+<html leng="en">
+<head>
+  
+
+	
+
+  <title></title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js"></script>
+   <script src="https://use.fontawesome.com/3556da0a2b.js"></script>
+  <script src="https://kit.fontawesome.com/6bea57b42a.js" crossorigin="anonymous"></script>
+  <link rel="stylesheet" type="text/css" href="BLOG/assets/style.css">
+  
+ 
+ 
+ 
+</head>
+<body>
+	<header>
+		<div class="container-fluid">
+	<nav class="navbar navbar-expand-sm navbar-dark bg-dark fixed-top">
+		<button class="navbar-toggler" data-toggle="collapse" data-target="#collapse_target">
+			<span class="navbar-toggler-icon"></span>
+		</button>
+		<div class="collapse navbar-collapse" id="collapse_target">
+			
+		<span class="navbar-text text-success ram"><span class="text-danger">Ram</span> Inspires</span>
+		<ul class="navbar-nav ml-auto">
+			<li class="nav-item">
+				<a class="nav-link text-white" href="#">Home</a>
+				 
+			</li>
+				<li class="nav-item">
+				<a href="#" class="nav-link text-white">About</a>
+			</li>
+			<li class="nav-item">
+				<a href="#" class="nav-link text-white">Services</a>
+			</li>
+				<li class="nav-item dropdown">
+				<a href="#" class="nav-link  dropdown-toggle text-white"data-toggle="dropdown"data-target="dropdown_target"> <i class="fa fa-user"></i>Ram Dhakal
+					<span class="caret"></span></a>
+			<div class="dropdown-menu" aria-labelledby="dropdown_target">
+				
+				<a class="dropdown-item" href="#">Dashbord</a>
+				<a class="dropdown-item text-danger" href="#">Logout</a>
+			
+		</div>
+	</li>
+</ul>
+</div>
+</nav>
+
+
+</div>
+
+
+
+<div class="container-fluid" style="margin-top:80px">
+<div class="row">
+<div class="col-md-8 bg-light border">
+   <div class="p-3">
+  <h2 class="text-center p-2">This is the title of the post</h2>
+
+  <p> 
+    North of the Greater Himalayas in western Nepal, ~6,100 metre Tibetan Border Ranges form the Ganges-Brahmaputra divide, which the international border generally follows. South of the Greater
+    Anapurna Himal is a subrange of the Himalaya in Nepal. It colies east of Dhaulagiri Himal and west of Mansiri Himal.Which the international border generally follows. South of the Greater Himalayas, Nepal has a High Mountain region.
+  </p>
+   <p>
+    North of the Greater Himalayas in western Nepal, ~6,100 metre Tibetan Border Ranges form the Ganges-Brahmaputra divide, which the international border generally follows. South of the Greater
+    Annapurna Himal is a subrange of the Himalaya in Nepal. It colies east of Dhaulagiri Himal and west of Mansiri Himal.Which the international border generally follows. South of the Greater Himalayas, Nepal has a High Mountain region.
+  </p>
+   <p>
+    North of the Greater Himalayas in western Nepal, ~6,100 metre Tibetan Border Ranges form the Ganges-Brahmaputra divide, which the international border generally follows. South of the Greater
+    Annapurna Himal is a subrange of the Himalaya in Nepal. It colies east of Dhaulagiri Himal and west of Mansiri Himal.Which the international border generally follows. South of the Greater Himalayas, Nepal has a High Mountain region.
+  </p>
+    <p>
+    North of the Greater Himalayas in western Nepal, ~6,100 metre Tibetan Border Ranges form the Ganges-Brahmaputra divide, which the international border generally follows. South of the Greater
+    Annapurna Himal is a subrange of the Himalaya in Nepal. It colies east of Dhaulagiri Himal and west of Mansiri Himal.Which the international border generally follows. South of the Greater Himalayas, Nepal has a High Mountain region.
+  </p>
+    <p>
+    North of the Greater Himalayas in western Nepal, ~6,100 metre Tibetan Border Ranges form the Ganges-Brahmaputra divide, which the international border generally follows. South of the Greater
+    Annapurna Himal is a subrange of the Himalaya in Nepal. It colies east of Dhaulagiri Himal and west of Mansiri Himal.Which the international border generally follows. South of the Greater Himalayas, Nepal has a High Mountain region.
+  </p>
+    <p>
+    North of the Greater Himalayas in western Nepal, ~6,100 metre Tibetan Border Ranges form the Ganges-Brahmaputra divide, which the international border generally follows. South of the Greater
+    Annapurna Himal is a subrange of the Himalaya in Nepal. It colies east of Dhaulagiri Himal and west of Mansiri Himal.Which the international border generally follows. South of the Greater Himalayas, Nepal has a High Mountain region.
+  </p>
+</div>
+</div>
+<div class="col-md-4 bg-light border"style="height:100%">
+  <div class=" pt-4">
+  <h2 class="pb-1">Popular</h2>
+  
+    <ul class="list-unstyled">
+      <li class="media">
+        <img src="assets/images/nepal2.jpg" class="mr-3" alt=""style="width:140px;height:100px">
+        <div class="media-body">
+          <h5 class="mt-0 mb-1"><a href="#">How to overcome your fears</a></h5>
+        </div>
+      </li>
+    </ul>
+
+  </div>
+    
+
+    <div>
+    <ul class="list-unstyled">
+      <li class="media">
+        <img src="assets/images/nepal2.jpg" class="mr-3" alt=""style="width:140px;height:100px">
+        <div class="media-body">
+          <h5 class="mt-0 mb-1"><a href="#">How to overcome your fears</a></h5>
+        </div>
+      </li>
+    </ul>
+
+  </div>
+    <div>
+    <ul class="list-unstyled">
+      <li class="media">
+        <img src="assets/images/nepal2.jpg" class="mr-3" alt=""style="width:140px;height:100px">
+        <div class="media-body">
+          <h5 class="mt-0 mb-1"><a href="#">How to overcome your fears</a></h5>
+        </div>
+      </li>
+    </ul>
+
+  </div>
+    <div>
+    <ul class="list-unstyled">
+      <li class="media">
+        <img src="assets/images/nepal2.jpg" class="mr-3" alt=""style="width:140px;height:100px">
+        <div class="media-body">
+          <h5 class="mt-0 mb-1"><a href="#">How to overcome your fears</a></h5>
+        </div>
+      </li>
+    </ul>
+  </div>
+
+  
+    <div>
+    <ul class="list-unstyled">
+      <li class="media">
+        <img src="assets/images/nepal2.jpg" class="mr-3" alt="" style="width:140px;height:100px">
+        <div class="media-body">
+          <h5 class="mt-0 mb-1"><a href="#">How to overcome your fears</a></h5>
+        </div>
+      </li>
+    </ul>
+
+
+        </div>
+      </div>
+    </div>
+  </div>
+
+
+  
+
+  
+  
+
+</div>
+<div class="col-md-4">
+        <h2>Topics</h2>
+        <div class="list-group" style="padding-bottom: 0px">
+          
+  
+  <a href="#" class="list-group-item list-group-item-action list-group-item-dark">Motivation</a>
+  <a href="#" class="list-group-item list-group-item-action list-group-item-dark">Inspiration</a>
+  <a href="#" class="list-group-item list-group-item-action list-group-item-dark">Life Lessons</a>
+  <a href="#" class="list-group-item list-group-item-action list-group-item-dark">Self Development</a>
+  <a href="#" class="list-group-item list-group-item-action list-group-item-dark">Biography</a>
+  <a href="#" class="list-group-item list-group-item-action list-group-item-dark">Poems</a>
+   <a href="#" class="list-group-item list-group-item-action list-group-item-dark">Quotes</a>
+</div>
+</div>
+</div>
+
+      
+        
+        <footer>
+       
+          <div class="row my-3 bg-dark">
+            <div class="col-md-4"style="height:100%">
+               <h5 class="text-success"><span class="text-danger">Ram</span>Inspires</h5>
+               <p class="text-white">RamInspires is a fictional blog in which posts are written and published by more than one author.</p>
+               <span class="text-white"><i class="fas fa-phone-alt"style="color:white"></i></i>&nbsp;+977-9863542563</span>
+               <br>
+               <span class="text-white"><i class="fas fa-envelope"style="color:white"></i>&nbsp;dhakalram144@gmail.com</span>
+             <br>
+             <div style="padding:10px">
+             <a href="#"><i class="fab fa-facebook" style="border:1px solid gray;width:35px;height:28px;padding-top: 5px;text-align: center"></i></a>&nbsp;
+                <a href="#"><i class="fab fa-instagram"style="border:1px solid gray;width:35px;height:28px;padding-top: 5px;text-align: center"></i></a>&nbsp;
+                 <a href="#"><i class="fab fa-twitter"style="border:1px solid gray;width:35px;height:28px;padding-top: 5px;text-align: center"></i></a>&nbsp;
+                 <a href="#"><i class="fab fa-youtube"style="border:1px solid gray;width:35px;height:28px;padding-top: 5px;text-align: center"></i></a>
+               </div>
+               </div>
+            
+             <div class="col-md-4 text-white"style="height:100%">
+             <h5 class="text-white">Quicks Links</h5>
+             <div class="links">
+             <ul><li><a href="#">Events</a></li>
+               <li><a href="#">Contact</a></li>
+                <li><a href="#">Mentors</a></li>
+                 <li><a href="#">Galleries</a></li>
+                  <li><a href="#">Write for us</a></li>
+                  <li><a href="#">Terms and conditions</a></li>
+                </ul>
+              </div>
+              </div>
+              <div class="col-md-4"style="height:100%">
+                <h5 class="text-white">Contact Us</h5>
+                
+                <form action="index.php" method="pos">
+                  <div class="form-group">
+                <input type="email" class="form-control my-1 bg-dark" placeholder="Your email address..." name="email">
+                <textarea name="message" class="form-control bg-dark" placeholder="Your message..."></textarea>
+                <button type="submit" class="btn btn-info pull-right" style="padding:.5;margin:.6rem "><i class="fas fa-envelope"></i> &nbsp;Send
+                </button>
+              </form>
+                
+              </div>
+          </div>
+        </div>
+      </footer>
+      
+
+      
+		
+
+                 
+      
+ 
+                
+                
+                 
+                 
+             
+
+         
+
+
+
+        
+
+
+</body>
+</html>
---
+++ single.php
@@ -0,0 +1,280 @@
+
+	<!DOCTYPE html>
+<html leng="en">
+<head>
+  <style type="text/css">
+    .ram{
+      font-family:arial;
+      font-size:150%; 
+    
+
+    }
+    
+    .dropdown:hover>.dropdown-menu{
+      display:block;
+    }
+    .carousel-inner img{
+      width:100%;
+      height:100%;
+      
+      
+    }
+    h2{
+      padding-top:10%;
+      
+    }
+    .btn{
+      padding-top: 0px;
+      margin-top: 0px;
+    }
+    
+        .ram1{
+      font-family:arial;
+      font-size:200%; 
+    }
+    .ram2{
+      margin-top:45px;
+      width:100%;
+      height:100%;
+      padding-left: 0px;
+    }
+    
+.links
+  a:hover{
+    color:white;
+    margin-left:15px;
+    transition: all .3s;
+
+  }
+.links a:link{
+  text-decoration: none;
+
+}
+.links a:visited{
+  color:white;
+}
+
+
+  </style>
+
+
+	
+
+  <title>Blog</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js"></script>
+   <script src="https://use.fontawesome.com/3556da0a2b.js"></script>
+  <script src="https://kit.fontawesome.com/6bea57b42a.js" crossorigin="anonymous"></script>
+  <link rel="stylesheet" type="text/css" href="BLOG/assets/style.css">
+  
+ 
+    <script type="text/javascript">
+          $(document).ready(function (){
+              $('#imageCarousel').carousel();
+          });
+      </script>
+ 
+</head>
+<body>
+	<header>
+		<div class="container-fluid">
+	<nav class="navbar navbar-expand-sm navbar-dark bg-dark fixed-top">
+		<button class="navbar-toggler" data-toggle="collapse" data-target="#collapse_target">
+			<span class="navbar-toggler-icon"></span>
+		</button>
+		<div class="collapse navbar-collapse" id="collapse_target">
+			
+		<span class="navbar-text text-success ram"><span class="text-danger">Ram</span> Inspires</span>
+		<ul class="navbar-nav ml-auto">
+			<li class="nav-item">
+				<a class="nav-link text-white" href="#">Home</a>
+				 
+			</li>
+				<li class="nav-item">
+				<a href="#" class="nav-link text-white">About</a>
+			</li>
+			<li class="nav-item">
+				<a href="#" class="nav-link text-white">Services</a>
+			</li>
+				<li class="nav-item dropdown">
+				<a href="#" class="nav-link  dropdown-toggle text-white"data-toggle="dropdown"data-target="dropdown_target"> <i class="fa fa-user"></i>Ram Dhakal
+					<span class="caret"></span></a>
+			<div class="dropdown-menu" aria-labelledby="dropdown_target">
+				
+				<a class="dropdown-item" href="#">Dashbord</a>
+				<a class="dropdown-item text-danger" href="#">Logout</a>
+			
+		</div>
+	</li>
+</ul>
+</div>
+</nav>
+ <div class="container-fluid">
+ 	<h2 class="text-center text-info">Trending Posts</h2>
+  <div class="carousel slide" data-ride="carousel" id="multi_item">
+    <div class="carousel-inner">
+      <div class="carousel-item active">
+        <div class="row">
+          <div class="col-sm"><img class="d-block w-100" src="assets/images/m1.jpg" alt="1 slide" class="img-responsive"></div>
+          <div class="col-sm"><img class="d-block w-100" src="assets/images/m2.jpg" alt="2 slide" class="img-responsive"></div>
+          <div class="col-sm"><img class="d-block w-100" src="assets/images/m3.jpg" alt="3 slide" class="img-responsive"></div>
+        </div>
+      </div>
+      <div class="carousel-item">
+        <div class="row">
+          <div class="col-sm"><img class="d-block w-100" src="assets/images/nepal5.jpg" alt="4 slide" class="img-responsive"></div>
+          <div class="col-sm"><img class="d-block w-100" src="assets/images/nepal6.jpg" alt="5 slide" class="img-responsive"></div>
+          <div class="col-sm"><img class="d-block w-100" src="assets/images/nepal7.jpg" alt="6 slide"class="img-responsive"></div>
+        </div>
+      </div>
+    </div>
+    <a class="carousel-control-prev" href="#multi_item" role="button" data-slide="prev">
+      <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+      <span class="sr-only">Previous</span>
+    </a>
+    <a class="carousel-control-next" href="#multi_item" role="button" data-slide="next">
+      <span class="carousel-control-next-icon" aria-hidden="true"></span>
+      <span class="sr-only">Next</span>
+    </a>
+  </div>
+</div>
+</div>
+</header>
+	<div class ="container-fluid bg-light my-3">
+    <div class="row">
+      <div class="col-md-8"style="height:200%">
+        
+        <ul class="list-unstyled">
+          <div><h2>Resent Posts</h2></div>
+  <li class="media">
+    <img src="assets/images/nepal7.jpg" class="mr-3" alt="...">
+    <div class="media-body">
+      <h5 class="mt-0 mb-1">Welcome to the Himalayan country of Nepal</h5>
+        <i class="fa fa-user"style="color:silver"></i>Ram Dhakal
+                     &nbsp;
+                        <i class="calander">Mar11,2020</i>
+      <p>North of the Greater Himalayas in western Nepal, ~6,100 metre Tibetan Border Ranges form the Ganges-Brahmaputra divide, which the international border generally follows. South of the Greater Himalayas, Nepal has a High Mountain region of ~4,000 metre.</p>
+       <a class="btn btn-primary pull-right" style="padding:.5rem 1rem" href="#">Read More</a>
+    </div>
+  </li>
+  <li class="media my-4">
+    <img src="assets/images/nepal2.jpg" class="mr-3" alt="...">
+    <div class="media-body">
+      <h5 class="mt-0 mb-1">Mountain ranges of Nepal </h5>
+       <i class="fa fa-user"style="color:silver"></i>Ram Dhakal
+                     &nbsp;
+                        <i class="calander">Feb12,2020</i>
+     <p>Annapurna Himal is a subrange of the Himalaya in Nepal. It colies east of Dhaulagiri Himal and west of Mansiri Himal.Which the international border generally follows. South of the Greater Himalayas, Nepal has a High Mountain region.</p>
+     <a class="btn btn-primary pull-right" style="padding:.5rem 1rem" href="#">Read More</a>
+    </div>
+  </li>
+  <li class="media">
+    <img src="assets/images/nepal3.jpg" class="mr-3" alt="...">
+    <div class="media-body">
+      <h5 class="mt-0 mb-1">Lumbini the Birthplace of Buddhism, Spiritual places in Nepal</h5>
+       <i class="fa fa-user"style="color:silver"></i>Ram Dhakal
+                     &nbsp;
+                        <i class="calander">Feb12,2020</i>
+      <p>Lumbini is the Buddha's birthplace, one of the world's most important spiritual sites and attracts Buddhist pilgrims from around the world. </p>
+       <a class="btn btn-primary pull-right" style="padding:.5rem 1rem" href="#">Read More</a>
+      
+    </div>
+  </li>
+</ul>
+      </div>
+        <div class="col-md-4"style="height:100%;padding-top:40px">
+           <h2>Search</h2>
+           <form class="form-inline" action="/action_page.php">
+
+    <input class="form-control mr-sm-2" type="text" placeholder="Search">
+    <button class="btn btn-success" type="submit">Search</button>
+  </form>
+        <h2>Topics</h2>
+        <div class="list-group" style="padding-bottom: 0px">
+          
+  
+  <a href="#" class="list-group-item list-group-item-action list-group-item-dark">Motivation</a>
+  <a href="#" class="list-group-item list-group-item-action list-group-item-dark">Inspiration</a>
+  <a href="#" class="list-group-item list-group-item-action list-group-item-dark">Life Lessons</a>
+  <a href="#" class="list-group-item list-group-item-action list-group-item-dark">Self Development</a>
+  <a href="#" class="list-group-item list-group-item-action list-group-item-dark">Biography</a>
+  <a href="#" class="list-group-item list-group-item-action list-group-item-dark">Poems</a>
+   <a href="#" class="list-group-item list-group-item-action list-group-item-dark">Quotes</a>
+</div>
+</div>
+</div>
+
+      
+        
+        <footer>
+       
+          <div class="row my-3 bg-dark">
+            <div class="col-md-4"style="height:100%">
+               <h5 class="text-success"><span class="text-danger">Ram</span>Inspires</h5>
+               <p class="text-white">RamInspires is a fictional blog in which posts are written and published by more than one author.</p>
+               <span class="text-white"><i class="fas fa-phone-alt"style="color:white"></i></i>&nbsp;+977-9863542563</span>
+               <br>
+               <span class="text-white"><i class="fas fa-envelope"style="color:white"></i>&nbsp;dhakalram144@gmail.com</span>
+             <br>
+             <div style="padding:10px">
+             <a href="#"><i class="fab fa-facebook" style="border:1px solid gray;width:35px;height:28px;padding-top: 5px;text-align: center"></i></a>&nbsp;
+                <a href="#"><i class="fab fa-instagram"style="border:1px solid gray;width:35px;height:28px;padding-top: 5px;text-align: center"></i></a>&nbsp;
+                 <a href="#"><i class="fab fa-twitter"style="border:1px solid gray;width:35px;height:28px;padding-top: 5px;text-align: center"></i></a>&nbsp;
+                 <a href="#"><i class="fab fa-youtube"style="border:1px solid gray;width:35px;height:28px;padding-top: 5px;text-align: center"></i></a>
+               </div>
+               </div>
+            
+             <div class="col-md-4 text-white"style="height:100%">
+             <h5 class="text-white">Quicks Links</h5>
+             <div class="links">
+             <ul><li><a href="#">Events</a></li>
+               <li><a href="#">Contact</a></li>
+                <li><a href="#">Mentors</a></li>
+                 <li><a href="#">Galleries</a></li>
+                  <li><a href="#">Write for us</a></li>
+                  <li><a href="#">Terms and conditions</a></li>
+                </ul>
+              </div>
+              </div>
+              <div class="col-md-4"style="height:100%">
+                <h5 class="text-white">Contact Us</h5>
+                
+                <form action="index.php" method="pos">
+                  <div class="form-group">
+                <input type="email" class="form-control my-1 bg-dark" placeholder="Your email address..." name="email">
+                <textarea name="message" class="form-control bg-dark" placeholder="Your message..."></textarea>
+                <button type="submit" class="btn btn-info pull-right" style="padding:.5;margin:.6rem "><i class="fas fa-envelope"></i> &nbsp;Send
+                </button>
+              </form>
+                
+              </div>
+          </div>
+        </div>
+      </footer>
+      
+
+      
+		
+
+                 
+      
+ 
+                
+                
+                 
+                 
+             
+
+         
+
+
+
+        
+
+
+</body>
+</html>
